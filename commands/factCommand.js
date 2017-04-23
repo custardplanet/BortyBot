8c161d29415239b927621bb426cc4b66e1393e16
Tom <tom563893@gmail.com>
2017-04-23 12:03:23 -0700

    Move support functions to API

--- commands/factCommand.js
+++ commands/factCommand.js
@@ -2,79 +2,10 @@
  * factCommand.js
  *
  * Say a random fact
-
- * If the code is doing something really stupid, it's because the Phantombot ORM
- * is REALLY bad and I didn't feel like overhauling it to develop this feature.
  *
  */
 (function() {
-    /* made up status codes because the shitty ORM doesn't return any -
-     * 0: add attempted and successful
-     * 1: value already in table
-     * 2: add attmpted and unsuccessful
-     */
-    function addValue(table, value) {
-        var id,
-            status = 0;
 
-        // make sure we don't insert duplicate rows because the shitty ORM doesn't check for you
-        if (getKeyOfValue(table, value) >= 0) {
-            status = 1;
-        } else {
-            // get max key and add 1 since you need to provide a key for the stupid set procedure
-            id = Math.max.apply(null, $.inidb.GetKeyList(table, '').map(Number)) + 1;
-            $.inidb.set(table, id, value);
-
-            if (getKeyOfValue(table, value) == -1) {
-                status = 2;
-            }
-        }
-
-        return status;
-    }
-
-    /* made up status codes because the shitty ORM doesn't return any -
-     * 0: del attempted and successful
-     * 1: value wasn't in table
-     * 2: del attmpted and unsuccessful
-     */
-    function delValue(table, value) {
-        var status = 0,
-            key = getKeyOfValue(table, value);
-
-        if (key == -1) {
-            status = 1;
-        } else {
-            $.inidb.del(table, key);
-
-            if (getKeyOfValue(table, value) >= 0) {
-                status = 2;
-            }
-        }
-
-        return status;
-    }
-
-    function getRandomKeyValue(table) {
-        var key = $.randElement($.inidb.GetKeyList(table, ''));
-        return [key, $.inidb.get(table, key)];
-    }
-
-    // support function to return key of a value because shitty ORM can't do this
-    function getKeyOfValue(table, value) {
-        var matches = $.inidb.searchByValue(table, value),
-            key = -1,
-            i;
-
-        for (i in matches) {
-            if ($.inidb.get(table, matches[i]) == value) {
-                key = matches[i];
-                break;
-            }
-        }
-
-        return key;
-    }
 
     $.bind('command', function(event) {
         var command = event.getCommand(),
@@ -93,7 +24,7 @@
 
         if (command.equalsIgnoreCase('catfact')) {
             if (!args[0]) {
-                value = getRandomKeyValue('catfacts');
+                value = $.getRandomKeyValue('catfacts');
                 $.say("Cat Fact #" + value[0] + ": " + value[1]);
             } else {
                 if (value = $.inidb.get('catfacts', args[0])) {
@@ -107,14 +38,14 @@
                 $.say("Usage: !addcatfact [catfact]");
             } else {
                 value = args.splice(0).join(' ');
-                $.say("\"" + value + "\" " + addStatusCodes[addValue('catfacts', value)] + "!");
+                $.say("\"" + value + "\" " + addStatusCodes[$.addValue('catfacts', value)] + "!");
             }
         } else if (command.equalsIgnoreCase('delcatfact')) {
             if (!args[0]) {
                 $.say("Usage: !delcatfact [catfact id]");
             } else {
                 value = args.splice(0).join(' ');
-                $.say("\"" + value + "\" " + delStatusCodes[delValue('catfacts', value)] + "!");
+                $.say("\"" + value + "\" " + delStatusCodes[$.delValue('catfacts', value)] + "!");
             }
         }
 
